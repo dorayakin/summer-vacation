3a0eb458ec46e2561b6d28fbd30aa4edb6a71e18
dorayakin <42695876+dorayakin@users.noreply.github.com>
2022-04-18 19:18:38 +0900

    Add user column and migration file

--- summervacation/diaryapp/admin.py
+++ summervacation/diaryapp/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import DiaryUser 
-# Register your models here
-class DiaryUserAdmin(admin.ModelAdmin):
-    list_display = ('username', )
-    search_fields = ('username', )
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(DiaryUser,DiaryUserAdmin)
+from .models import DiaryUser
+from . import models
+# Register your models here
+
+
+class DiaryUserAdmin(UserAdmin):
+    list_display = ('class_id', 'username',
+                    'last_name', 'first_name', 'is_active', 'last_login')
+    list_display_links = ('username',)
+    ordering = ('class_id', )
+    search_fields = ('username', 'class_id')
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {
+         "fields": ("class_id", "first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(DiaryUser, DiaryUserAdmin)
--- summervacation/summervacation/urls.py
+++ summervacation/summervacation/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('diaryapp/', include('diaryapp.urls')),
+    path('diaryapp/', include('diaryapp.urls')),
 
 ]
