a486739326d5492677e2fe7d15af74387f7a680c
dorayakin <42695876+dorayakin@users.noreply.github.com>
2022-06-27 00:10:50 +0900

    Add bootstrap for login,logout,home

--- summervacation/summervacation/settings.py
+++ summervacation/summervacation/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'diaryapp.apps.DiaryappConfig',
 
 ]
@@ -134,3 +135,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
